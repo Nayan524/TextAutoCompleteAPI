0e9f9eb86268c6d097f358d790cb10d76798aede
Nayan <ngodhwan@usc.edu>
2025-05-13 12:23:59 -0700

    Added validations for user prompt

--- TextAutoCompleteAPI/Models/AutoComplete.cs
+++ TextAutoCompleteAPI/Models/AutoComplete.cs
@@ -1,10 +1,19 @@
-﻿namespace TextAutoCompleteAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+
+namespace TextAutoCompleteAPI.Models
 {
     public class AutoComplete
+
     {
+        [Required(ErrorMessage = "Input cannot be empty.")]
+        [MinLength(3, ErrorMessage = "Input must be at least 3 characters.")]
+        [MaxLength(100, ErrorMessage = "Input must not exceed 200 characters.")]
         public string Input { get; set; }
 
-        public string getInput()
+        public string GetInput()
         {
             return $"{Input}";
         }
