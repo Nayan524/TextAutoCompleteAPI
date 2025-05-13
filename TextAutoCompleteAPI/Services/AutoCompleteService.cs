7dd282a3b5ea9ed0e7882794abccfc87d1f04f5a
Nayan <ngodhwan@usc.edu>
2025-05-13 10:24:55 -0700

    Created an interface for Services

---
+++ TextAutoCompleteAPI/Services/AutoCompleteService.cs
@@ -0,0 +1,9 @@
+﻿using TextAutoCompleteAPI.Models;
+
+namespace TextAutoCompleteAPI.Services
+{
+    public interface AutoCompleteService
+    {
+        Task<List<string>> GetSuggestions(AutoComplete auto);
+    }
+}
