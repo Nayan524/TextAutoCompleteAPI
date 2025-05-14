50b11bdb7a996fd8707ae4cf683be98fc8de90ae
Nayan <ngodhwan@usc.edu>
2025-05-13 17:27:51 -0700

    Rename the conroller class

---
+++ TextAutoCompleteAPI/Controllers/AutoCompleteController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextAutoCompleteAPI.Models;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+using System.Threading.Tasks;
+using OpenAI.Interfaces;
+using TextAutoCompleteAPI.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace TextAutoCompleteAPI.Controllers
+{
+    [Route("api/auto")]
+    [ApiController]
+    public class AutoCompleteController : ControllerBase
+    {
+        private readonly IAutoCompleteService _autoCompleteService;
+
+        public AutoCompleteController(IAutoCompleteService autoCompleteService)
+        {
+            _autoCompleteService = autoCompleteService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuto([FromBody] AutoComplete auto)
+
+        {
+            Console.WriteLine(auto.GetInput());
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var suggestions = await _autoCompleteService.GetSuggestions(auto);
+                return Ok(new { suggestions });
+
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+           
+        }
+    }
+}
