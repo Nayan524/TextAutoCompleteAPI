4a451c429a67435d1f5cf36f411399629933ce62
Nayan <ngodhwan@usc.edu>
2025-05-13 17:41:05 -0700

    Added comments

--- TextAutoCompleteAPI/Services/IAutoCompleteService.cs
+++ TextAutoCompleteAPI/Services/IAutoCompleteService.cs
@@ -2,6 +2,7 @@
 
 namespace TextAutoCompleteAPI.Services
 {
+    //Creating interface for Service Class. Multiple Service class can exist which implements the interface. 
     public interface IAutoCompleteService
     {
         Task<List<string>> GetSuggestions(AutoComplete auto);
