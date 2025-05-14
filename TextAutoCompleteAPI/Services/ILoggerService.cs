3963add3efc048b526fae6a3f60179a390492d28
Nayan <ngodhwan@usc.edu>
2025-05-13 20:16:45 -0700

    Implemented Logger Service

---
+++ TextAutoCompleteAPI/Services/ILoggerService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public interface ILoggerService
+{
+    Task LogApi(string input, int statusCode, string message = "");
+}
---
+++ TextAutoCompleteAPI/Services/LoggerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class LoggerService : ILoggerService
+{
+
+    private readonly string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "api_logs.txt");
+
+    public LoggerService()
+    {
+        var directory = Path.GetDirectoryName(logFilePath);
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+    }
+
+    public async Task LogApi(string input, int statusCode, string message = "")
+    {
+        // Creating the log message
+        var logMessage = $"{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} - Input: {input}, StatusCode: {statusCode}, Message: {message}\n";
+
+        // Add the message to the log file
+        await File.AppendAllTextAsync(logFilePath, logMessage);
+    }
+}
