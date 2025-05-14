6fcc77071b489151c735082487afcbf5b2ac88ab
Nayan <ngodhwan@usc.edu>
2025-05-13 19:33:54 -0700

    Modified service class to include cache implementation

--- TextAutoCompleteAPI/Services/AutoCompleteServiceAPI.cs
+++ TextAutoCompleteAPI/Services/AutoCompleteServiceAPI.cs
@@ -4,6 +4,7 @@ using OpenAI.ObjectModels.RequestModels;
 using TextAutoCompleteAPI.Models;
 using System.Net.Http;
 using System.Text.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace TextAutoCompleteAPI.Services
 {
@@ -12,17 +13,24 @@ namespace TextAutoCompleteAPI.Services
 
         //Declaring HttpClient to call NLP Service
         private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _memoryCache;
 
-        //Below is known as Constructor Dependency Injection which creates instance of HttpClient at runtime. 
-        public AutoCompleteServiceAPI(HttpClient httpClient)
+        //Below is known as Constructor Dependency Injection which creates instance of HttpClient and InMemory cache at runtime. 
+        public AutoCompleteServiceAPI(HttpClient httpClient, IMemoryCache memoryCache)
         {
             _httpClient = httpClient;
+            _memoryCache = memoryCache;
         }
 
         //Defining the method to process the user prompt and returing the suggestions
         public async Task<List<string>> GetSuggestions(AutoComplete auto)
         {
             var prompt = Uri.EscapeDataString(auto.GetInput());
+            
+            //To check if we have cached suggestions for the prompt. 
+            if (_memoryCache.TryGetValue(prompt, out List<string> cachedSuggestions))
+                return cachedSuggestions;
+            
             var url = $"https://api.datamuse.com/sug?s={prompt}";
 
             var response = await _httpClient.GetAsync(url);
@@ -52,10 +60,17 @@ namespace TextAutoCompleteAPI.Services
                 Console.WriteLine("Results is null");
             }
 
-            return results?
+            var suggestionText = results?
                 .Select(r => r.Word)
                 .Take(5)
                 .ToList() ?? new List<string>();
+
+            //Cache the results with a 5 minute expiration
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+
+            _memoryCache.Set(prompt, suggestionText, cacheEntryOptions);
+            return suggestionText;
 
         }
 
